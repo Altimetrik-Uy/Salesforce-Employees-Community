8389c38911cb485b511f4a7b20f94524d816370a
strellesAltimetrik <strelles@altimetrik.com>
2020-10-22 15:18:53 -0300

    Done.
    The query has been fixed to have the last Status and only one Status
    
    Fixed some code error, create test class.
    
    SEC-7 metadata site
    
    Metadata site

--- force-app/main/default/lwc/employeeOverallSubtab/employeeOverallSubtab.js
+++ force-app/main/default/lwc/employeeOverallSubtab/employeeOverallSubtab.js
@@ -3,7 +3,9 @@ import strUserId from '@salesforce/user/Id';
 import TrafficLightGreen from '@salesforce/resourceUrl/TrafficLightGreen';
 import TrafficLightRed from '@salesforce/resourceUrl/TrafficLightRed';
 import TrafficLightYellow from '@salesforce/resourceUrl/TrafficLightYellow';
+import TrafficLightBalck from '@salesforce/resourceUrl/TrafficLightBlack';
 import getProjectStatusImageName from '@salesforce/apex/LWCEmployeeOverallController.getProjectStatusImageName';
+import getStatusImageName from '@salesforce/apex/LWCEmployeeOverallController.getStatusImageName';
 
 export default class EmployeeOverallSubtab extends LightningElement {
     @api employeeId;
@@ -15,7 +17,45 @@ export default class EmployeeOverallSubtab extends LightningElement {
         { fieldName: 'ProjectImageSrc', type:'image' },
         { fieldName: 'ProjectImageAlt' }
     ];
+    @track imageStatus;
+    @track statusTableColumns = [
+        { label: 'Profile Pic', fieldName: 'AssetImageSrc', type:'image'},
+        { label: 'alt', fieldName: 'AssetImageAlt'},
+    ];
 
+    @wire(getStatusImageName,{uId: '$userId'}) wiredImage({error,data}){
+        let TrafficLight;
+        let id = 0;
+        if (data) {
+            this.imageStatus = data;
+            let preparedAssets = [];
+            let preparedAsset = {};
+            preparedAsset.Id = id;
+            TrafficLight = TrafficLightRed;
+            preparedAsset.AssetImageAlt = data;
+                switch(data){
+                    case 'Red':
+                        TrafficLight = TrafficLightRed;
+                        break;
+                    case 'Yellow':
+                        TrafficLight = TrafficLightYellow;
+                        break;
+                    case 'Green':
+                        TrafficLight = TrafficLightGreen;
+                        break;
+                    case 'Black':
+                        TrafficLight = TrafficLightBalck;
+                        break;
+                }
+            preparedAsset.AssetImageAlt = data;
+            preparedAsset.AssetImageSrc = TrafficLight;
+            preparedAssets.push(preparedAsset);
+            this.imageStatus = preparedAssets;
+        } else if (error) {
+            this.error = error;
+        }
+    }
+    
     @wire(getProjectStatusImageName,{uId: '$userId'}) wiredProjectStatusImageName({error,data}){
         if (data) {
             this.projectStatusImageName = data;
