ed1d45247318accc1ef9902cd4a59ae29a30233a
Nelson Sáenz <nsaenz@altimetrik.com>
2020-11-13 14:53:22 -0500

    (SEC-15): Career Path - Table Component

--- force-app/main/default/lwc/salesforceCertificationDetailTable/salesforceCertificationDetailTable.js
+++ force-app/main/default/lwc/salesforceCertificationDetailTable/salesforceCertificationDetailTable.js
@@ -1,48 +1,30 @@
-import { LightningElement, track, wire } from 'lwc';
-import strUserId from '@salesforce/user/Id';
-import getTrailBlazerId from '@salesforce/apex/LWCEmployeeOverallController.getTrailBlazerId';
+import { LightningElement, track } from 'lwc';
 import getSalesforcePath from '@salesforce/apex/SalesforcePathByRole.getSalesforcePath'
 import genericErrorMessage_lbl from '@salesforce/label/c.GenericErrorMessage_lbl';
 
 export default class SalesforceCertificationDetailTable extends LightningElement {
-    roles;
-    @track userId = strUserId;
-    @track trailBlazerId;
-    @track spinnerOn = true;
+    @track roles = undefined;
+
     // error message, when set will render the error panel
-    errorMsg;
+    @track errorMsg;
 
     connectedCallback(){
         getSalesforcePath()
-        .then(response => {
+        .then(response=>{
             if(response){
                 this.roles = response;
             }else{
 
             }
         })
-        .catch(error => {
-            this.errorMsg = error.body.message;
+        .catch(error=>{
+            console.log('Error -->'+error);
+            this.errorMsg = error;
         })
     }
 
     get genericErrorMessage() {
+        // note: you can store this in a custom label if you need
         return genericErrorMessage_lbl;
-    }
-
-    toggleSpinner(status) {
-        this.spinnerOn = status;
-    }
-    @wire(getTrailBlazerId, {uId: '$userId'})
-    wiredTrailblazer({ error, data }) {
-        if (data) {
-            this.trailBlazerId = data;
-            this.toggleSpinner(false);
-        } else if (error) {
-            this.trailBlazerId = undefined;
-        }else {
-            if (data !== undefined)
-                this.toggleSpinner(false);
-        }
     }
 }
