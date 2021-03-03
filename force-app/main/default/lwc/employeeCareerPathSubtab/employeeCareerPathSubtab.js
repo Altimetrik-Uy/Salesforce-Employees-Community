57139f7c7d6a65544e6a36eeaec76b0007bd89e7
Javier Luces <jluces@altimetrik.com>
2021-03-03 15:00:23 -0300

    Request Role Change Button added

--- force-app/main/default/lwc/employeeCareerPathSubtab/employeeCareerPathSubtab.js
+++ force-app/main/default/lwc/employeeCareerPathSubtab/employeeCareerPathSubtab.js
@@ -1,5 +1,49 @@
-import {LightningElement, api} from 'lwc';
+import {LightningElement, api, wire, track} from 'lwc';
+import sendMessage from '@salesforce/apex/LWCEmployeeCareerPathController.sendMessage';
+import getManager from '@salesforce/apex/LWCEmployeeCareerPathController.getManager';
+import getUser from '@salesforce/apex/LWCEmployeeCareerPathController.getUser';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class EmployeeCareerPathSubtab extends LightningElement {
     @api employeeid;
+    @track error; 
+    @track lstManagerId = [];
+    @track userName = '';
+
+    @wire(getManager,{empId: '$employeeid'}) getManager({error,data}){
+        if(data){
+            this.lstManagerId = data;
+        }else if (error){
+            this.error = error;
+        }
+    }
+
+    @wire(getUser,{empId: '$employeeid'}) getUser({error,data}){
+        if(data){
+            this.userName = data;
+        }else if (error){
+            this.error = error;
+        }
+    }
+    
+    onClickSendMessage(){
+        sendMessage({lstManagersId:this.lstManagerId, userName:this.userName})
+        .then (s=>{
+            if(s){
+                const event = new ShowToastEvent({
+                    title: 'Success!',
+                    message: 'Message has been sent.',
+                    variant: 'success',
+                });
+                this.dispatchEvent(event);
+            }else{
+                const event = new ShowToastEvent({
+                    title: 'Fail!',
+                    message: 'Message has not been sent. Current project does not have a manager assigned to be notified.',
+                    variant: 'error',
+                });
+                this.dispatchEvent(event);
+            }
+        })
+    }
 }
