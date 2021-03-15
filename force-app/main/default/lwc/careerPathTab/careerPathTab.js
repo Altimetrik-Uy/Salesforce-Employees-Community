4b86285d315c1b1430f378c4e428c10b3223c024
lmiraballes <lmiraballes@altimetrik.com>
2021-03-15 10:43:51 -0700

    default role in carrer path tab

--- force-app/main/default/lwc/careerPathTab/careerPathTab.js
+++ force-app/main/default/lwc/careerPathTab/careerPathTab.js
@@ -2,6 +2,7 @@ import { LightningElement, api, wire, track} from 'lwc';
 import sendMessage from '@salesforce/apex/LWCEmployeeCareerPathController.sendMessage';
 import getManager from '@salesforce/apex/LWCEmployeeCareerPathController.getManager';
 import getUser from '@salesforce/apex/LWCEmployeeCareerPathController.getUser';
+import getRole from '@salesforce/apex/LWCEmployeeCareerPathController.getRole';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 const ACTIVE_CLASS = ' slds-is-active';
 const NAV_ITEM_CLASS = 'slds-vertical-tabs__nav-item';
@@ -9,7 +10,7 @@ const NAV_ITEM_CLASS = 'slds-vertical-tabs__nav-item';
 export default class CareerPathTab extends LightningElement {
     @api employeeid;
     @api mainRole = "Developer Path";
-    selectedrole;
+    @track selectedrole;
     @track error; 
     @track lstManagerId = [];
     @track userName = '';
@@ -78,6 +79,14 @@ export default class CareerPathTab extends LightningElement {
     @wire(getUser,{empId: '$employeeid'}) getUser({error,data}){
         if(data){
             this.userName = data;
+        }else if (error){
+            this.error = error;
+        }
+    }
+
+    @wire(getRole,{empId: '$employeeid'}) getRole({error,data}){
+        if(data){
+            this.selectedrole = data;
         }else if (error){
             this.error = error;
         }
