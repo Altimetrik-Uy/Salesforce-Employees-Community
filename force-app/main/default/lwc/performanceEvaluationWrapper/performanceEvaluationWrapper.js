65a52bd67589708bcefb09b1a07ea871e7f2bbc3
lmacea <68128589+lmacea@users.noreply.github.com>
2021-05-13 15:55:46 -0500

    SEC-127

---
+++ force-app/main/default/lwc/performanceEvaluationWrapper/performanceEvaluationWrapper.js
@@ -0,0 +1,14 @@
+import { api,wire, LightningElement } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import STATUS_FIELD from '@salesforce/schema/Review__c.Status__c';
+
+export default class PerformanceEvaluationWrapper extends LightningElement {
+    @api 
+    recordId;
+    @wire(getRecord, { recordId:'$recordId', fields: [STATUS_FIELD], optionalFields: [] })
+    review;
+    get reviewOpen() {
+        let status = getFieldValue(this.review.data, STATUS_FIELD);
+        return status == 'Open' || status == 'In Progress';         
+    }
+}
