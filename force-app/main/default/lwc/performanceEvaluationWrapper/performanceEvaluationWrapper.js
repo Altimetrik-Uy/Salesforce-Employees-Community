79922d9e1cf6cd476095fb7f4285162cbf01ae3c
lmacea <68128589+lmacea@users.noreply.github.com>
2021-05-17 17:09:40 -0500

    SEC-124

--- force-app/main/default/lwc/performanceEvaluationWrapper/performanceEvaluationWrapper.js
+++ force-app/main/default/lwc/performanceEvaluationWrapper/performanceEvaluationWrapper.js
@@ -3,23 +3,20 @@ import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import STATUS_FIELD from '@salesforce/schema/Review__c.Status__c';
 
 export default class PerformanceEvaluationWrapper extends LightningElement {
-    @api recordId;
-    @api filterStatus;
+    @api 
+    recordId;
     @wire(getRecord, { recordId:'$recordId', fields: [STATUS_FIELD], optionalFields: [] })
     review;
-
-    get reviewOpen() {    
+    get reviewOpen() {
         let status = getFieldValue(this.review.data, STATUS_FIELD);
-        console.log('status {'+ status+'}');
-        console.log('recordId {'+ this.recordId+'}');
         return status == 'Open' || status == 'In Progress';         
     }
 
     handleClick() {
         this.dispatchEvent(new CustomEvent('back'));
     }
-
-    handleSubmit(){
-        this.dispatchEvent(new CustomEvent('submit'));
+    
+    saveComments(){
+        this.template.querySelector("c-review-comments").saveForm();
     }
 }
