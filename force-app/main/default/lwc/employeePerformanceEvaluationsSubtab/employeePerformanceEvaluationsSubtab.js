2528aebd4fac995d675a419ed4a56019606ca352
gabriel fernandez <feesgabriel@gmail.com>
2021-06-03 16:28:58 -0300

    employeePerformanceEvaluationController and EvaluationSub updated

--- force-app/main/default/lwc/employeePerformanceEvaluationsSubtab/employeePerformanceEvaluationsSubtab.js
+++ force-app/main/default/lwc/employeePerformanceEvaluationsSubtab/employeePerformanceEvaluationsSubtab.js
@@ -17,6 +17,9 @@ export default class EmployeePerformanceEvaluationsSubtab extends LightningEleme
     @track paginationRange = [];
     @track totalRecords;
     valueFilter = 'Reviews about me';
+    isReviewSelected = false;
+    @api projectInformation;
+    @api reviewSelected;
 
     get optionsFilter() {
         return [
@@ -46,7 +49,7 @@ export default class EmployeePerformanceEvaluationsSubtab extends LightningEleme
                             if (this.valueFilter == 'Reviews done' || this.valueFilter == 'Reviews to do'   ) {
                                 this.projectStatuses.forEach(asset => {
                                     let preparedAsset = {};
-                                    preparedAsset.Id = id++;
+                                    preparedAsset.Id = asset.Review__c;
                                     preparedAsset.Status = asset.Review__r.Status__c;
                                     preparedAsset.StatusDate = asset.Review__r.CreatedDate;
                                     preparedAsset.StatusComments = asset.Review__r.Comments__c;
@@ -55,27 +58,16 @@ export default class EmployeePerformanceEvaluationsSubtab extends LightningEleme
                                 });
                                 this.projectStatuses = preparedAssets;
                             }else{
-                                const map = new Map();
-                                this.projectStatuses.forEach(asset => {                 
-                                    if(map.has(asset.Review__c)){
-                                        let preparedAssetval= map.get(asset.Review__c);
-
-                                        preparedAssetval.ManagerName = preparedAssetval.ManagerName +' - '+asset.Evaluator__r.Name;
-                                        map.set(asset.Review__c, preparedAssetval);
-                                    }
-                                    else{
-                                        let preparedAsset = {};
-                                        preparedAsset.Id = id++;
-                                        preparedAsset.Status = asset.Review__r.Status__c;
-                                        preparedAsset.StatusDate = asset.Review__r.CreatedDate;
-                                        preparedAsset.StatusComments = asset.Review__r.Comments__c;
-                                        preparedAsset.ManagerName = asset.Evaluator__r.Name;
-                                        map.set(asset.Review__c, preparedAsset);
-
-                                    }                                    
+                                this.projectStatuses.forEach(asset => {
+                                    let preparedAsset = {};
+                                    preparedAsset.Id = asset.Review__c;
+                                    preparedAsset.Status = asset.Status__c;
+                                    preparedAsset.StatusDate = asset.CreatedDate;
+                                    preparedAsset.StatusComments = asset.Comments__c;
+                                    preparedAsset.ManagerName = asset.CreatedBy.Name;
+                                    preparedAssets.push(preparedAsset);
                                 });
-
-                                this.projectStatuses = map.values();
+                                this.projectStatuses = preparedAssets;
                             }
                         } else if (error) {
                             this.error = error;
@@ -100,6 +92,22 @@ export default class EmployeePerformanceEvaluationsSubtab extends LightningEleme
 
     }
 
+    handleWrapper(event){
+        let newSelection = event.currentTarget.dataset.id;
+        this.reviewSelected = newSelection;
+
+        if (this.isReviewSelected){
+            this.isReviewSelected = false;
+        }
+        else {
+            this.isReviewSelected = true;            
+        }
+    }
+
+    handleClick(event){
+       
+    }
+
    
     get projectOptions() {
         return this.projectItem;
@@ -117,27 +125,17 @@ export default class EmployeePerformanceEvaluationsSubtab extends LightningEleme
                         if (data) {
                             this.projectStatuses = data;
                             let preparedAssets = [];
-                            let id = 0;
-                            const map = new Map();
+                            //let id = 0;
                             this.projectStatuses.forEach(asset => {
-                                if(map.has(asset.Review__c)){
-                                    let preparedAssetval= map.get(asset.Review__c);
-
-                                    preparedAssetval.ManagerName = preparedAssetval.ManagerName +' - '+asset.Evaluator__r.Name;
-                                    map.set(asset.Review__c, preparedAssetval);
-                                }
-                                else{
-                                    let preparedAsset = {};
-                                    preparedAsset.Id = id++;
-                                    preparedAsset.Status = asset.Review__r.Status__c;
-                                    preparedAsset.StatusDate = asset.Review__r.CreatedDate;
-                                    preparedAsset.StatusComments = asset.Review__r.Comments__c;
-                                    preparedAsset.ManagerName = asset.Evaluator__r.Name;
-                                    map.set(asset.Review__c, preparedAsset);
-
-                                }         
+                                let preparedAsset = {};
+                                preparedAsset.Id = asset.Id;
+                                preparedAsset.Status = asset.Status__c;
+                                preparedAsset.StatusDate = asset.CreatedDate;
+                                preparedAsset.StatusComments = asset.Comments__c;
+                                preparedAsset.ManagerName = asset.CreatedBy.Name;
+                                preparedAssets.push(preparedAsset);
                             });
-                            this.projectStatuses = map.values();
+                            this.projectStatuses = preparedAssets;
                         } else if (error) {
                             this.error = error;
                         }
@@ -195,26 +193,16 @@ export default class EmployeePerformanceEvaluationsSubtab extends LightningEleme
                         });
                         this.projectStatuses = preparedAssets;
                     }else{
-                        const map = new Map();
                         this.projectStatuses.forEach(asset => {
-                            if(map.has(asset.Review__c)){
-                                let preparedAssetval= map.get(asset.Review__c);
-
-                                preparedAssetval.ManagerName = preparedAssetval.ManagerName +' - '+asset.Evaluator__r.Name;
-                                map.set(asset.Review__c, preparedAssetval);
-                            }
-                            else{
-                                let preparedAsset = {};
-                                preparedAsset.Id = id++;
-                                preparedAsset.Status = asset.Review__r.Status__c;
-                                preparedAsset.StatusDate = asset.Review__r.CreatedDate;
-                                preparedAsset.StatusComments = asset.Review__r.Comments__c;
-                                preparedAsset.ManagerName = asset.Evaluator__r.Name;
-                                map.set(asset.Review__c, preparedAsset);
-
-                            }        
+                            let preparedAsset = {};
+                            preparedAsset.Id = id++;
+                            preparedAsset.Status = asset.Status__c;
+                            preparedAsset.StatusDate = asset.CreatedDate;
+                            preparedAsset.StatusComments = asset.Comments__c;
+                            preparedAsset.ManagerName = asset.CreatedBy.Name;
+                            preparedAssets.push(preparedAsset);
                         });
-                        this.projectStatuses = map.values();
+                        this.projectStatuses = preparedAssets;
                     }
                 } else if (error) {
                     this.error = error;
--- force-app/main/default/lwc/performanceEvaluationWrapper/performanceEvaluationWrapper.js
+++ force-app/main/default/lwc/performanceEvaluationWrapper/performanceEvaluationWrapper.js
@@ -1,7 +1,6 @@
 import { api,wire, LightningElement } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import STATUS_FIELD from '@salesforce/schema/Review__c.Status__c';
-import getEmployeeReviewItem from '@salesforce/apex/LWCPerformanceEvaluationController.getEmployeeReviewItem';
 
 export default class PerformanceEvaluationWrapper extends LightningElement {
     @api recordId;
@@ -10,18 +9,9 @@ export default class PerformanceEvaluationWrapper extends LightningElement {
     review;
 
     get reviewOpen() {    
-        let status;
-        getEmployeeReviewItem({ reviewId: this.recordId, filterStatus : this.filterStatus })
-        .then((result) => {
-            this.review =  result;
-                        console.log('review '+ this.review.Status__c);
-                        console.log('review '+ this.review.Comments__c);
-                        status = this.review.Status__c;
-        })
-        .catch((error) => {           
-            console.log('error '+ error.name);
-        });
-        
+        let status = getFieldValue(this.review.data, STATUS_FIELD);
+        console.log('status {'+ status+'}');
+        console.log('recordId {'+ this.recordId+'}');
         return status == 'Open' || status == 'In Progress';         
     }
 
